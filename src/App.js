f91b965b4ba4ccced7154a8f4a75636872009277
sebasman15 <sebastiansmit1995@gmail.com>
2023-10-29 00:23:06 +0200

    made background and 2nd section and scrolling action

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import HamburgerMenu from './HamburgerMenu';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="section1">
         <HamburgerMenu />
-        <h1>Welcome to My Landing Page</h1>
-        <p>This is a simple landing page created with React.</p>
+        {/* <h1>Welcome to My Landing Page</h1> */}
       </header>
+      
+      <section className="section2">
+        <h2>New Content Here</h2>
+        <p>This is some additional content that appears as you scroll.</p>
+      </section>
     </div>
   );
 }
--- src/HamburgerMenu.js
+++ src/HamburgerMenu.js
@@ -14,6 +14,7 @@ const HamburgerMenu = () => {
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
             <li><a href="#">Contact</a></li>
           </ul>
         </nav>
